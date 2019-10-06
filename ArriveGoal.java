4412bf2fa7b71892c85f6235d8f20de78163b0a6
Modjo AI <r.meuter@hotmail.com>
2019-10-06 19:21:43 +0200

    Ajout des comportement d'arrive au but et de la demande de reperage dans l'espace

---
+++ ArriveGoal.java
@@ -0,0 +1,49 @@
+package ProjetGOT;
+
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.navigation.MovePilot;
+import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
+
+public class ArriveGoal implements Behavior{
+
+	private EV3ColorSensor cs;
+	private MovePilot pilot;
+	private Carte carte;
+	private CalibrageColor colorTab;
+
+	public ArriveGoal (EV3ColorSensor cs, MovePilot pilot, Carte carte, CalibrageColor colorTab) {
+		this.cs = cs;
+		this.pilot = pilot;
+		this.carte= carte;
+		this.colorTab = colorTab;
+	}
+	
+	@Override
+	public boolean takeControl() {
+		// TODO Auto-generated method stub
+		// Le but est une case soit rouge (camp) soit blanc (ville)
+		return  carte.isArriveGoal(); //(colorTab.getCalibreColor(cs) == "rouge"||colorTab.getCalibreColor(cs) == "blanc") &&
+	}
+
+	@Override
+	public void action() {
+		pilot.stop();
+		LCD.drawString("Tu es arrivée mamène", 0, 3);
+		LCD.drawString("Remet moi à la case de départ, poto", 0, 4);
+		Button.waitForAnyPress();
+		LCD.clear();
+		// Redifinition du goal et de la position initial
+		carte.setGoal(2);
+		carte.setPositionHistorique(carte.getDebut());
+	}
+
+	@Override
+	public void suppress() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ WhereIAm.java
@@ -0,0 +1,36 @@
+package ProjetGOT;
+
+import java.util.Arrays;
+
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
+
+public class WhereIAm implements Behavior {
+	Carte carte;
+	
+	public WhereIAm (Carte carte) {
+		this.carte = carte;
+	}
+
+	@Override
+	public boolean takeControl() {
+		// TODO Auto-generated method stub
+		return Button.RIGHT.isDown();
+	}
+
+	@Override
+	public void action() {
+		LCD.drawString(Arrays.toString(carte.getPositionHistorique()), 0, 0);
+		Delay.msDelay(1000);
+		LCD.clear();
+	}
+
+	@Override
+	public void suppress() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
