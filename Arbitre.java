5438a1208dac4081d645faca2fc86fc3e4991925
Modjo AI <r.meuter@hotmail.com>
2019-10-06 19:20:38 +0200

    Ajout du bluetooth

--- Arbitre.java
+++ Arbitre.java
@@ -17,56 +17,68 @@ public class Arbitre {
 	public static void main(String[] args) {
 		Button.RIGHT.waitForPressAndRelease();
 		
-		// Base
+		//######################## Base
 		Arbitrator arby = null;
 		Carte carte = null;
+		Bluetooth bl = null;
 		
-		// Definition des senseurs
+		//######################## Definition des senseurs
 		EV3ColorSensor cs = new EV3ColorSensor(SensorPort.S3);
 		
 		
-		// Calibrage des couleurs
+		//######################## Calibrage des couleurs
 		LCD.drawString("Calibrons", 0, 0);
 		Button.waitForAnyEvent();
 		LCD.clear();
 		CalibrageColor colorTab = new CalibrageColor();
 		colorTab.Calibrage(cs);
 		
-		// Definition du camp
+		//######################## Definition du camp avec choix d'utiliser le bluetooth
 		LCD.drawString("Choisis ton camp", 0, 0);
-		LCD.drawString("Haut pour Sauvageons", 0, 1);
-		LCD.drawString("Reste des bouttons garde de nuit", 0, 2);
+		LCD.drawString("H/G pour Sauvageons", 0, 1);
+		LCD.drawString("B/reste pour garde de nuit", 0, 2);
+		LCD.drawString("Les boutons de coté D/G sont pour le bleutooth et autre ss bluetooth ", 0, 3);
 		Button.waitForAnyPress();
+		
 		if (Button.UP.isDown()) {
 			carte = new Carte(true);
 			LCD.drawString("Sauvageon", 0, 0);
+		} else if (Button.DOWN.isDown()) {
+			carte = new Carte(false);
+			LCD.drawString("Garde de la nuit", 0, 0);
+		} else if (Button.LEFT.isDown()) {
+			carte = new Carte(true);
+			LCD.drawString("Sauvageon", 0, 0);
+			bl = new Bluetooth(true);
 		} else {
 			carte = new Carte(false);
 			LCD.drawString("Garde de la nuit", 0, 0);
+			bl = new Bluetooth(false);
 		}
 		Delay.msDelay(300);
 		LCD.clear();
 		
-		// definition du chassis
+		//######################## definition du chassis
 		Wheel wheel1 = WheeledChassis.modelWheel(Motor.B, 56.).offset(-60);
 		Wheel wheel2 = WheeledChassis.modelWheel(Motor.C, 56.).offset(60);
 		Chassis chassis = new WheeledChassis(new Wheel[]{wheel1, wheel2}, 2);	
 		MovePilot pilot = new MovePilot(chassis);
 		
-		//Alteration de la vitesse de base
+		//######################## Alteration de la vitesse de base
 		pilot.setLinearSpeed(50.);
 		pilot.setAngularSpeed(50.);
 		
 		
-		// Definition des comportements
+		//######################## Definition des comportements
 		Behavior b1 = new ArriveGoal(cs, pilot, carte, colorTab);
 		Behavior b2 = new DetectCouleur(cs, pilot, carte, colorTab);
 		Behavior b3 = new LigneNoire(cs, pilot, carte, colorTab);
 		Behavior b4 = new ArretBouton(arby, cs, pilot);
+		
 		Behavior[] bArray = {b2,b3,b1,b4}; // du moins prioritaire au plus prioritaire
 		arby = new Arbitrator(bArray);
 		
-		// Arret de l'arbitre
+		//######################## Arret de l'arbitre
 		if(b4 instanceof ArretBouton) {
 			ArretBouton b = (ArretBouton)b4;
 			b.setArbitrator(arby);
