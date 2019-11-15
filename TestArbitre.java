40791e116f50806b4c24df5312e05a3a8e22c94e
LLEJEUNE1 <55869258+LLEJEUNE1@users.noreply.github.com>
2019-11-15 18:25:18 +0100

    Déplacements okay, plus court chemin okay

--- TestArbitre.java
+++ TestArbitre.java
@@ -10,29 +10,32 @@ public class TestArbitre {
 		Button.waitForAnyPress();
 		
 //##### Initialisation de l'arbitre, du biais et du robot ####
-		Button.waitForAnyPress();
-		int newBiaisAngle = -13;
+
+		short newBiaisAngle = -0;
 		RobotNavigator robotNav = new RobotNavigator (newBiaisAngle);
-		LCD.drawString("Robot crée", 0, 0);
-		LinkedList <Integer> chemin = new LinkedList <Integer>();
-		chemin.add(RobotNavigator.SUD);
-		chemin.add(RobotNavigator.SUD);
-		chemin.add(RobotNavigator.EST);
-		chemin.add(RobotNavigator.SUD);
-		chemin.add(RobotNavigator.NORD);
+		byte goal = robotNav.getGoal();
+		byte position = robotNav.getPosition();
 		
-		for (int x : chemin){
+		Dijkstra dijkstra = new Dijkstra(position, goal);
+		
+		System.out.println(position);
+		Button.waitForAnyPress();
+		System.out.println(goal);
+		Button.waitForAnyPress();
+		LinkedList <Short> chemin = dijkstra.dijkstra();
+		
+		System.out.println(chemin);
+		Button.waitForAnyPress();
+		
+		LCD.clear();
+		
+		for (short x : chemin){
 			LCD.drawInt(robotNav.versDirection(x), 0, 0);
 			if (robotNav.versDirection(x) == 0){
 				robotNav.avance(x);
-				if (robotNav.getEtape() == 3){
-					robotNav.sarreteNSeconde();
-				}
 			}else{
 				robotNav.tourne(x);
-				if (robotNav.getEtape() == 3){
-					robotNav.sarreteNSeconde();
-				}
+				robotNav.avance(x);
 			}
 		}
 		
